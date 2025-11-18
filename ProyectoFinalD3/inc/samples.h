ef94e3bec78279d89dd71b31b73612cf66174fe4
Ciro Tonini <ciro.tonini@mi.unc.edu.ar>
2025-11-18 15:43:28 -0300

    Update samples.h

--- ProyectoFinalD3/inc/samples.h
+++ ProyectoFinalD3/inc/samples.h
@@ -5,10 +5,19 @@
 
 extern const uint8_t sampleS5[];
 extern const uint8_t sampleS1[];
+extern const uint8_t sampleS2[];
+extern const uint8_t sampleS3[];
+extern const uint8_t sampleS4[];
+extern const uint8_t sampleS6[];
 
 
 extern const uint32_t SAMPLE_S5_LEN;
 extern const uint32_t SAMPLE_S1_LEN;
+extern const uint32_t SAMPLE_S2_LEN;
+extern const uint32_t SAMPLE_S3_LEN;
+extern const uint32_t SAMPLE_S4_LEN;
+extern const uint32_t SAMPLE_S6_LEN;
+
 
 
 #endif
